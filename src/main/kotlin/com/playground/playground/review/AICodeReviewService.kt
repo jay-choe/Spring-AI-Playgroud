f227a88a5487e2dd67eaaca53548a2239301650d
jay-choe <jay0764@naver.com>
2025-04-13 00:45:49 +0900

    refactor: remove debug code

--- src/main/kotlin/com/playground/playground/review/AICodeReviewService.kt
+++ src/main/kotlin/com/playground/playground/review/AICodeReviewService.kt
@@ -53,8 +53,6 @@ class AICodeReviewService(
         // preprocess with LLM
         val preprocessed = preProcessingTemplate.replace("{code}", code)
         val processedCode = llmQueryService.query(preprocessed)
-        println("Done preprocess")
-        println(processedCode)
         val context = ragService.retrieve(code)
         val reviewRequest = reviewTemplate.replace("{code}", processedCode)
                                           .replace("{context}", context)
@@ -62,53 +60,4 @@ class AICodeReviewService(
         val review = llmQueryService.streamResponseQuery(reviewRequest)
         return review
     }
-
-    @PostConstruct
-    fun init() {
-        val result = review("""
-            이 코드를 리뷰 받고 싶은데..
-            어떻게 하면 좋을까..?
-            package com.example.distributedlock.service;
-
-            import java.util.concurrent.TimeUnit;
-            import lombok.RequiredArgsConstructor;
-            import org.redisson.api.RLock;
-            import org.redisson.api.RedissonClient;
-            import org.springframework.stereotype.Service;
-
-            @Service
-            @RequiredArgsConstructor
-            public class RedissonLockService {
-
-                private final StockService stockService;
-                private final RedissonClient redissonClient;
-
-                private static final int WAIT_TIME = 5;
-                private static final int OCCUPATION_TIME = 1;
-                public void increaseStock(long key) {
-                    RLock lock = redissonClient.getLock(key + "");
-
-                    try {
-                        boolean success = lock.tryLock(WAIT_TIME, OCCUPATION_TIME, TimeUnit.SECONDS);
-
-                        if (!success) {
-                            // retry -> just test code.. (recursion)
-                            increaseStock(key);
-                        }
-
-                        stockService.increaseStockFrom(key, 1);
-
-                    } catch (Exception e) {
-                        // Do Nothing
-                    } finally {
-                        lock.unlock();
-                    }
-
-                }
-            }
-        """.trimIndent())
-        println(result.collectList().block())
-    }
-
-
 }
